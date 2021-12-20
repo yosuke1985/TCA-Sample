9c2accee737baaeb687c3c60e85b4940050ab071
Yosuke NAKAYAMA <ihatenonoildressing@gmail.com>
2021-12-20 18:17:34 +0900

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Yosuke NAKAYAMA on 2021/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SampleAppOfTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAppOfTCAApp.swift
+//  Shared
+//
+//  Created by Yosuke NAKAYAMA on 2021/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SampleAppOfTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
